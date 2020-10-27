0f2d985bfe5c9cf05f75f180a97ee8e1bf274a5d
BackdoorIncorp <backdoorincorparation@gmail.com>
2020-10-27 14:34:32 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home : new MyApplication()
+  ));
+}
+class MyApplication extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+
+}
+class _State extends State<MyApplication> {
+  
+  void showBottom() {
+    showModalBottomSheet <void>(context: context, builder: (BuildContext context) {
+      return new Container(
+        padding: new EdgeInsets.all(15.0),
+        child: new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          new Text("Some info here",style: new TextStyle(color: Colors.red,fontWeight: FontWeight.bold),),
+          new RaisedButton(onPressed: () => Navigator.pop(context),child: new Text("Close"),)
+        ],  
+        ),
+      );
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Name here"),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text("Hello World"),
+              new RaisedButton(onPressed: showBottom, child: new Text("Open"),)
+            ],
+          ),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
